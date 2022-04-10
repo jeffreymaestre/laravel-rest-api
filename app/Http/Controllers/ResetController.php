bd8f7487d66e47f5cacb5f071720c0081bf4bc4b
jeffreymaestre <36870162+jeffreymaestre@users.noreply.github.com>
2022-04-10 17:54:03 -0500

    Agregado el endpoint reset

--- app/Http/Controllers/ResetController.php
+++ app/Http/Controllers/ResetController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 
 class ResetController extends Controller
 {
-    //
+    public function reset(){
+        Artisan::call('migrate:fresh');
+        
+        return 'OK'; //TODO: status code is 200
+    }
 }
